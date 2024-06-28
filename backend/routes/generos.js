0fc2989237784da840b53fd789f203ae3407eaaa
luciagiampieri <96505@sistemas.frc.utn.edu.ar>
2024-06-28 17:29:40 -0300

    Cambios

--- backend/routes/generos.js
+++ backend/routes/generos.js
@@ -6,16 +6,16 @@ const router = express.Router();
 // GET ALL de la tabla GENEROS que tiene los atributos id y nombre.
 router.get("/api/generos", async function (req, res, next) {
       try {
-          let data = await db.Generos.findAll({
-              attributes: ["id", "nombre"],
-          });
-          res.json(data);
+            let data = await db.Generos.findAll({
+                  attributes: ["id", "nombre"],
+            });
+            res.json(data);
       } catch (error) {
-          console.error("Error al obtener los géneros:", error);
-          res.status(500).json({ error: "Error al obtener los géneros" });
+            console.error("Error al obtener los géneros:", error);
+            res.status(500).json({ error: "Error al obtener los géneros" });
       }
 });
-  
+
 // ejemplo de uso: http://localhost:4444/api/generos
 
 module.exports = router;
